93d9061ebd1faa71647bfb9b1f48cc19218e770b
raghavdotcom <drraghavsethi@gmail.com>
2024-02-07 04:01:15 -0500

    api interaction

--- RickAndMorty/ApiClient/RMRequest.swift
+++ RickAndMorty/ApiClient/RMRequest.swift
@@ -8,5 +8,58 @@
 import Foundation
 
 final class RMRequest {
+    // Base URL
+    // Endpoint
+    // Path Params
+    // Query Parameters
     
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endpoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    /// Composes url
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public let httpMethod = "GET"
+    
+    init(
+        endpoint: RMEndpoint,
+        pathComponents: [String] = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -15,17 +15,15 @@ final class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character,
+            pathComponents: ["1"]
+        )
+        
+        print(request.url)
+        
+        RMService.shared.execute(<#T##request: RMRequest##RMRequest#>, 
+                                 expecting: <#T##(Decodable & Encodable).Protocol#>, completion: <#T##(Result<String, Error>) -> Void#>)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
